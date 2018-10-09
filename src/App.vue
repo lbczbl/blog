26dadfe6ca500bc20cf8591100c455a172551439
lbc <2553486226@qq.com>
2018-10-09 08:10:30 +0800

    showblog

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-   <add-blog></add-blog>
+   <!-- <add-blog></add-blog> -->
+   <show-blogs></show-blogs>
   </div>
 </template>
 
 <script>
 import AddBlog from './components/AddBlog'
+import ShowBlogs from './components/ShowBlogs'
 
 export default {
   name: 'App',
   components: {
-    AddBlog
+    AddBlog,
+    ShowBlogs
   }
 }
 </script>
