b3dd904fa8ab630289f46a5f971f53fbabcad599
lbc <2553486226@qq.com>
2018-10-09 10:20:22 +0800

    addrouter

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
 import App from './App'
+import Routes from './routes'
 
 Vue.config.productionTip = false
 
 Vue.use(VueResource)
+Vue.use(VueRouter)
 
 // 自定义指令
 // Vue.directive('rainbow',{
@@ -38,9 +41,16 @@ Vue.filter("snippet",function(value) {
   return value.slice(0,100)+ "...";
 })
 
+
+//创建路由
+const router=new VueRouter({
+  routes:Routes,
+  mode:"history"
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  router:router
 })
